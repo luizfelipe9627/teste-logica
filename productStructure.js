f29f88349a9c265de7cd99d55a0fa941b0cb998f
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-05-26 21:51:06 -0300

    update

--- productStructure.js
+++ productStructure.js
@@ -10,9 +10,27 @@ const products = [
   "azul-XG",
   "azul-XG",
   "azul-XG",
-  "azul-P"
-]
+  "azul-P",
+];
 
 module.exports = () => {
-	// Seu código vai aqui!
-}
+  const productsObject = {}; // Criado um objeto vazio chamado productsObject que vai armazenar os produtos convertidos.
+
+  // O for of passa por cada item da array products.
+  for (const item of products) {
+    const [cor, tamanho] = item.split("-"); // Criado duas arrays, uma chamada cor e a outro chamada tamanho, para cada array o split divide a string do item atual em duas partes, sendo cor e tamanho e armazena nas arrays criadas, quando tiver o separador("-").
+
+    // Faz uma verificação, se a cor atual já existe no objeto productsObject, caso não existir, executa o if.
+    if (!productsObject[cor]) {
+      productsObject[cor] = {}; // Insere dentro da cor atual do objeto productsObject um objeto vazio.
+    } // Se o tamanho não existir dentro da cor atual executa o if.
+    if (!productsObject[cor][tamanho]) {
+      productsObject[cor][tamanho] = 1; // Adiciona na cor atual o valor 1 no tamanho da cor atual.
+    } // Senão se o tamanho da cor atual já existir executa o else.
+    else {
+      productsObject[cor][tamanho] += 1; // Incrementa(soma com o que já tiver) no productsObject o valor 1 ao valor de tamanho já existente.
+    }
+  }
+
+  return productsObject; // Retorna o objeto com as cores e todos seus tamanhos.
+};
